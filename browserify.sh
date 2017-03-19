7a37ce9ed9b3544d74feb1b822e9b29b937981a4
ErikK <erik@transferwise.com>
2017-03-19 10:03:22 +0200

    add browserify script

---
+++ browserify.sh
@@ -0,0 +1,1 @@
+browserify index.js -o index.bundle.js
--- install_npm_modules.sh
+++ install_npm_modules.sh
@@ -2,3 +2,4 @@ echo "Installing necessary npm modules"
 npm install bitcore-lib --save
 npm install bitcore-payment-protocol --save
 npm install axios --save
+npm install -g browserify
